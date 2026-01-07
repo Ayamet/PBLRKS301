7ef5c16139197f5ed12574a6019c45a6701d8199
Ayamet <ayamet2029@gmail.com>
2026-01-07 11:22:11 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
-from nemukerja.app import create_app
+from nemukerja import create_app, db
 import os
+
 app = create_app()
 
-if __name__ == '__main__':
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
